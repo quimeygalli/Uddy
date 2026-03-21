ef626112d5dd346ddfc285424dcae9e2fcd5a4f4
Quimey <quimey2000@gmail.com>
2026-03-21 01:11:39 +0000

    Created components folder and handled signup form

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,63 +1,10 @@
-// TODO; send data from form to backend
-
 import "./index.css";
+import SignUpForm from "./components/signUp";
 
 function App() {
-  async function testRequest() {
-    const res = await fetch("http://127.0.0.1:8000/api/signup/", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        username: "test123",
-        email: "test@test.com",
-        password: "12345678",
-        repeat_password: "12345678",
-      }),
-    });
-
-    const data = await res.json();
-    console.log(data);
-  }
-
   return (
     <div>
-      <h1 className="text-3xl  font-bold  text-amber-50">Sign up</h1>
-      <form className="pt-20 flex items-center justify-center ">
-        <div className="flex flex-col  gap-2 w-50 pb-4 text-amber-50">
-          <label className="">Username</label>
-          <input
-            className="border border-zinc-400 rounded-md bg-amber-50"
-            type="text"
-            name="username"
-          />
-          <label>Email</label>
-          <input
-            className="border border-zinc-400 rounded-md bg-amber-50"
-            type="text"
-            name="username"
-          />
-          <label>Password</label>
-          <input
-            className="border border-zinc-400 rounded-md bg-amber-50"
-            type="text"
-            name="username"
-          />
-          <label>Repeat Password</label>
-          <input
-            className="border border-zinc-400 rounded-md bg-amber-50"
-            type="text"
-            name="username"
-          />
-          <button
-            className="border text-cyan-950 border-zinc-400 rounded-md p-1 w-25 bg-cyan-200"
-            onClick={testRequest}
-          >
-            Sign up
-          </button>
-        </div>
-      </form>
+      <SignUpForm />
     </div>
   );
 }
---
+++ frontend/src/components/signUp.jsx
@@ -0,0 +1,79 @@
+import { useState } from "react";
+import "../index.css";
+
+// Apparently React functions must start with an uppercase
+function SignUpForm() {
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+    repeat_password: "",
+  });
+
+  const handleData = (data) => {
+    setFormData({
+      ...formData,
+      // [] make JS evaluate the expression before inserting the result.
+      [data.target.name]: data.target.value, // Very interesting React behavior.
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(formData);
+    // TODO; submit data to backend
+  };
+
+  return (
+    <div>
+      <h1 className="text-3xl  font-bold  text-amber-50">Sign up</h1>
+      <form
+        onSubmit={handleSubmit}
+        className="pt-20 flex items-center justify-center "
+      >
+        <div className="flex flex-col  gap-2 w-50 pb-4 text-amber-50">
+          <label className="">Username</label>
+          <input
+            required
+            onChange={handleData}
+            className="form_fields"
+            type="text"
+            name="username"
+            placeholder="John"
+          />
+          <label>Email</label>
+          <input
+            required
+            onChange={handleData}
+            className="form_fields"
+            type="text"
+            name="email"
+            placeholder="john@example.com"
+          />
+          <label>Password</label>
+          <input
+            required
+            onChange={handleData}
+            className="form_fields"
+            type="password"
+            name="password"
+            placeholder="Min 8 char."
+          />
+          <label>Repeat Password</label>
+          <input
+            required
+            onChange={handleData}
+            className="form_fields"
+            type="password"
+            name="repeat_password"
+            placeholder="Just repeat the pw"
+          />
+          <button className="border text-cyan-950 border-zinc-400 rounded-md p-1 w-25 bg-cyan-200">
+            Sign up
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
+export default SignUpForm;
