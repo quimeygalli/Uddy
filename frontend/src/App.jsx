cd2211c68df725c1fb895b8c9001aed8aa80a50a
Quimey <quimey2000@gmail.com>
2026-03-27 13:11:22 -0300

    Modified file paths, names.
    Made it so the sidebar only appears in the right pages (not in signup and signin).

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,15 +2,23 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
 import SignUpForm from "./pages/signUp.jsx";
 import SignInForm from "./pages/signIn.jsx";
-import Homepage from "./pages/homepage.jsx";
+import Homepage from "./pages/HomePage.jsx";
+import AddSubject from "./pages/AddSubject.jsx";
+import MainLayout from "./layout/MainLayout.jsx";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        {/* Does not have a sidebar */}
         <Route path="/signup" element={<SignUpForm />} />
         <Route path="/signin" element={<SignInForm />} />
-        <Route path="/" element={<Homepage />} />
+
+        {/* Has a sidebar */}
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/add-subject" element={<AddSubject />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
